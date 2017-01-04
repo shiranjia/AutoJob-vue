af4b813d2f123d15ed5a96e1e1885d2751b708d2
jiashiran <jiashiran@jd.com>
2017-01-04 17:09:52 +0800

    数据绑定

--- src/components/Job.vue
+++ src/components/Job.vue
@@ -1,7 +1,13 @@
-<template xmlns:v-on="http://www.w3.org/1999/xhtml">
+<template xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-bind="http://www.w3.org/1999/xhtml">
   <div  class="jumbotron">
+    <select  @change="chooseJob($event)">
+      <option v-for="(job, _index) in jobs" v-bind:value="job.Name" >
+        {{ job.Name }}
+      </option>
+    </select>
     <div v-for="(job, _index) in jobs">
-      <div :id="_index" class="job">
+      <transition name="bounce">
+      <div :id="_index" class="job" v-show="job.show">
         <H2>JobName: {{job.Name}}</H2><br>
         <div>
           ssh.userName: <input v-model="job.Config.User">
@@ -10,50 +16,54 @@
         </div>
         <br>
         <div>
-          本地命令 (执行路径;命令;参数(参数用空格隔开) example:E:\github\ticket.h5\web;mvn;clean package -Dmaven.test.skip=true -P artifactory,development -Dfile.encoding=UTF-8 ):
-          <button v-on:click="addlB()">ADD</button>
+          本地命令 (执行路径 命令 参数 example:E:\github\ticket.h5\web;mvn clean package -Dmaven.test.skip=true -P artifactory,development -Dfile.encoding=UTF-8 ):
+          <button v-on:click="addlB(_index)">ADD</button>
           <br>
           <ul><li v-for="(lb, lb_index) in job.LocalBefore">
-            Path<input v-model="lb.Path" size="50">
-            Command<input v-model="lb.Command" size="50">
-            Args<input v-model="lb.Args" size="50">
-            <button v-on:click="removelB(lb_index)">X</button>
+            路径:<input v-model="lb.Path" size="30">
+            命令:<input v-model="lb.Command" size="30">
+            参数:<input v-model="lb.Args" size="50">
+            <button v-on:click="removelB(_index,lb_index)">X</button>
           </li></ul>
         </div>
         <div>
-          远程命令<button v-on:click="addrB()">ADD</button>:<br>
+          远程命令<button v-on:click="addrB(_index)">ADD</button>:<br>
           <ul>
-            <li v-for="(rb, index) in job.remoteBefore">
-              <input v-model="rb.text" size="150"><button v-on:click="removerB(index)">X</button>
+            <li v-for="(rb, index) in job.RemoteBefore">
+              命令:<input v-model="rb.Command" size="150"><button v-on:click="removerB(_index,index)">X</button>
             </li>
           </ul>
         </div>
         <br>
         <div>
-          上传文件: 本地目录：<input v-model="job.localPath" size="70"> 远程目录：<input v-model="job.remotePath" size="70">
+          上传文件: 本地目录：<input v-model="job.UploadJob.LocalPath" size="70"> 远程目录：<input v-model="job.UploadJob.RemotePath" size="70">
         </div>
         <br>
         <div>
-          上传文件后本地命令 (执行路径;命令;参数(参数用空格隔开) example:E:\github\ticket.h5\web;mvn;clean package -Dmaven.test.skip=true -P artifactory,development -Dfile.encoding=UTF-8 ):
-          <button v-on:click="addlA()">ADD</button>
+          上传文件后本地命令 (执行路径 命令 参数 example:E:\github\ticket.h5\web mvn clean package -Dmaven.test.skip=true -P artifactory,development -Dfile.encoding=UTF-8 ):
+          <button v-on:click="addlA(_index)">ADD</button>
           <br>
-          <ul><li v-for="(la, index) in job.localAfter">
-            <input v-model="la.text" size="150"><button v-on:click="removelA(index)">X</button>
+          <ul><li v-for="(la, index) in job.LocalAfter">
+            路径:<input v-model="la.Path" size="30">
+            命令:<input v-model="la.Command" size="30">
+            参数:<input v-model="la.Args" size="50">
+            <button v-on:click="removelA(_index,index)">X</button>
           </li></ul>
         </div>
         <div>
-          上传文件后远程命令<button v-on:click="addrA()">ADD</button>:<br>
+          上传文件后远程命令<button v-on:click="addrA(_index)">ADD</button>:<br>
           <ul>
-            <li v-for="(ra, index) in job.remoteAfter">
-              <input v-model="ra.text" size="150"><button v-on:click="removerA(index)">X</button>
+            <li v-for="(ra, index) in job.RemoteAfter">
+              <input v-model="ra.Command" size="150"><button v-on:click="removerA(_index,index)">X</button>
             </li>
           </ul>
         </div>
         <br>
         <div>
-          <button>deploy</button><button>update</button><button>remove</button>
+          <button>deploy</button><button v-on:click="update(_index)">update</button><button>remove</button>
         </div>
       </div>
+      </transition>
     </div>
   </div>
 </template>
@@ -61,17 +71,13 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 Vue.use(VueResource)
+Vue.http.options.emulateHTTP = true
 export default {
-  props: {
-    ip: {
-      type: String,
-      default: 'text'
-    }
-  },
   data () {
     return {
       jobs: [
         {
+          show: true,
           Name: '',
           Config: {
             User: '192.168.1.1',
@@ -90,60 +96,101 @@ export default {
               Args: ['clean']
             }
           ],
-          remoteBefore: [
-                   { text: 'Learn JavaScript' },
-                   { text: 'Learn Vue.js' },
-                   { text: 'Build Something Awesome' }],
-          localPath: '',
-          remotePath: '',
-          localAfter: [
-                   { text: 'Learn JavaScript' },
-                   { text: 'Learn Vue.js' },
-                   { text: 'Build Something Awesome' }],
-          remoteAfter: [
-                    { text: 'Learn JavaScript' },
-                    { text: 'Learn Vue.js' },
-                    { text: 'Build Something Awesome' }],
+          RemoteBefore: [
+                   { Command: 'Learn JavaScript' },
+                   { Command: 'Learn Vue.js' },
+                   { Command: 'Build Something Awesome' }
+          ],
+          UploadJob: {
+            LocalPath: '',
+            RemotePath: ''
+          },
+          LocalAfter: [
+            {
+              Path: '/home',
+              Command: 'free',
+              Args: ['-a']
+            }
+          ],
+          RemoteAfter: [
+                    { Command: 'Learn JavaScript' },
+                    { Command: 'Learn Vue.js' },
+                    { Command: 'Build Something Awesome' }],
           msg: ''
         }
       ]
     }
   },
   methods: {
-    goBack: function (e) {
-      console.log('a')
+    addlB: function (_index) {
+      if (this.jobs[_index].LocalBefore == null) {
+        this.jobs[_index].LocalBefore = [{Path: '/path', Command: 'command', Args: ['args...']}]
+      } else {
+        this.jobs[_index].LocalBefore.push({Path: '/path', Command: 'command', Args: ['args...']})
+      }
     },
-    addlB: function () {
-      this.localBefore.push({ text: '' })
+    removelB: function (_index, index) {
+      this.jobs[_index].LocalBefore.splice(index, 1)
     },
-    removelB: function (index) {
-      this.localBefore.splice(index, 1)
+    addrB: function (_index) {
+      if (this.jobs[_index].RemoteBefore == null) {
+        this.jobs[_index].RemoteBefore = [{ Command: '' }]
+      } else {
+        this.jobs[_index].RemoteBefore.push({ Command: '' })
+      }
     },
-    addrB: function () {
-      this.remoteBefore.push({ text: '' })
+    removerB: function (_index, index) {
+      this.jobs[_index].RemoteBefore.splice(index, 1)
     },
-    removerB: function (index) {
-      this.remoteBefore.splice(index, 1)
+    addlA: function (_index) {
+      if (this.jobs[_index].LocalAfter == null) {
+        this.jobs[_index].LocalAfter = [{Path: '/path', Command: 'command', Args: ['args...']}]
+      } else {
+        this.jobs[_index].LocalAfter.push({Path: '/path', Command: 'command', Args: ['args...']})
+      }
     },
-    addlA: function () {
-      this.localAfter.push({ text: '' })
+    removelA: function (_index, index) {
+      this.jobs[_index].LocalAfter.splice(index, 1)
     },
-    removelA: function (index) {
-      this.localAfter.splice(index, 1)
+    addrA: function (_index) {
+      if (this.jobs[_index].RemoteAfter == null) {
+        this.jobs[_index].RemoteAfter = [{ Command: '' }]
+      } else {
+        this.jobs[_index].RemoteAfter.push({ Command: '' })
+      }
     },
-    addrA: function () {
-      this.remoteAfter.push({ text: '' })
+    removerA: function (_index, index) {
+      this.jobs[_index].RemoteAfter.splice(index, 1)
     },
-    removerA: function (index) {
-      this.remoteAfter.splice(index, 1)
+    chooseJob: function (event) {
+      let index = event.explicitOriginalTarget.index
+      for (let i = 0; i < this.jobs.length; i++) {
+        this.jobs[i].show = false
+      }
+      // console.log(this.jobs[index].show)
+      this.jobs[index].show = true
+      this.addlA(this.jobs.length - 1)
+      this.removelA(this.jobs.length - 1, this.jobs[this.jobs.length - 1].LocalAfter.length - 1)
+    },
+    update: function (_index) {
+      // console.log(this.jobs[_index])
+      let json = JSON.stringify(this.jobs[_index])
+      console.log(json)
+      this.$http.get('http://localhost/rest/saveOrUpdate', {params: {job: json}}).then((response) => {
+        // 响应成功回调
+        console.log(response)
+      }, (response) => {
+        // 响应错误回调
+        console.log(response)
+      })
     }
   },
   created () {
-    Vue.http.get('http://localhost/rest?sad', {foo: 'bar'}).then((response) => {
+    Vue.http.get('http://localhost/rest', {name: 'bar'}).then((response) => {
       let JS = JSON.parse(response.data)
       let jobs = JS.JS.jobs
-      // this.$set(this, 'jobName', jobs[0].Name)
       this.$set(this, 'jobs', jobs)
+      jobs[0].show = true
     }, (response) => { // error callback
     })
   }
@@ -167,5 +214,34 @@ li {
 
 a {
   color: #42b983;
+}
+
+.bounce-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-out .5s;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes bounce-out {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(0);
+  }
 }
 </style>
